69fb6e9dc765d0d302aad83ac44ec2f51bf51dcf
Ala Shaabana <ala@bittensor.com>
2024-04-01 12:04:35 -0700

    Added stagenet genesis

--- node/src/command.rs
+++ node/src/command.rs
@@ -45,6 +45,7 @@ impl SubstrateCli for Cli {
     fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
         Ok(match id {
             "local" => Box::new(chain_spec::localnet_config()?),
+            "stage" => Box::new(chain_spec::stagenet_config()?),
             "finney" => Box::new(chain_spec::finney_mainnet_config()?),
             "" | "test_finney" => Box::new(chain_spec::finney_testnet_config()?),
             path => Box::new(chain_spec::ChainSpec::from_json_file(
